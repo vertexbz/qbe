564ff03650e3ef7e3c0310944d16af8592e6233f
vertex <adam.makswiej@gmail.com>
2023-06-10 02:01:13 +0200

    nicer paths

--- qbe/bin/paths.py
+++ qbe/bin/paths.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
-import pprint
 import qbe.cli as cli
 from qbe.config import Config
+
+
+def _print(paths: dict, indent=0):
+    for k, v in paths.items():
+        if isinstance(v, dict):
+            print('  ' * indent + k + ':')
+            _print(v, indent=indent + 1)
+        else:
+            print('  ' * indent + k + ': ' + cli.message_important(v))
 
 
 @cli.command(short_help='Display paths')
 @cli.pass_config
 def paths(config: Config):
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(config.paths.__dict__)
+    print('config.paths:')
+    _print(config.paths.__dict__, indent=1)
