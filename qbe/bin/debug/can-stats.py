39f11e6009c818c0a03d4a64af8ba0b0f5ce9aa1
vertex <adam.makswiej@gmail.com>
2023-06-11 01:17:54 +0200

    ui tweaks

--- qbe/bin/debug/can-stats.py
+++ qbe/bin/debug/can-stats.py
@@ -4,6 +4,10 @@ from tabulate import tabulate
 import qbe.cli as cli
 
 
+def intfmt(val: int) -> str:
+    return format(val, ',')
+
+
 @cli.command(short_help='Display can stats')
 def can_stats():
     stats = {k: v for k, v in psutil.net_io_counters(pernic=True).items() if k.startswith('can')}
@@ -14,13 +18,13 @@ def can_stats():
     for iface, s in stats.items():
         table = [
             [cli.bold(iface), 'Bytes', 'Packets', cli.error('Errors'), cli.error('Dropped')],
-            ['Received', s.bytes_recv, s.packets_recv,
-             cli.error(str(s.errin)) if s.errin > 0 else '0',
-             cli.error(str(s.dropin)) if s.dropin > 0 else '0',
+            ['Received', intfmt(s.bytes_recv), intfmt(s.packets_recv),
+             cli.error(intfmt(s.errin)) if s.errin > 0 else '0',
+             cli.error(intfmt(s.dropin)) if s.dropin > 0 else '0',
              ],
-            ['Sent', s.bytes_sent, s.packets_sent,
-             cli.error(str(s.errout)) if s.errout > 0 else '0',
-             cli.error(str(s.dropout)) if s.dropout > 0 else '0'
+            ['Sent', intfmt(s.bytes_sent), intfmt(s.packets_sent),
+             cli.error(intfmt(s.errout)) if s.errout > 0 else '0',
+             cli.error(intfmt(s.dropout)) if s.dropout > 0 else '0'
              ]
         ]
         print(tabulate(table, headers='firstrow', tablefmt='rounded_grid',
