d8cce1aa3492295512129ebd7fe9d61ce5672acc
vertex <adam.makswiej@gmail.com>
2023-06-10 02:28:42 +0200

    canboot

--- qbe/bin/debug/canboot-list.py
+++ qbe/bin/debug/canboot-list.py
@@ -1,8 +1,23 @@
 from __future__ import annotations
+
+import asyncio
+
 import qbe.cli as cli
-from qbe.support import Sudo
+from qbe.support.canboot import CanSocket
 
 
-@cli.command(short_help='Lists canboot devices')
-def canboot_list():
-    pass
+@cli.command(short_help='Lists CanBoot devices')
+@cli.option('--interface', default='can0')
+def canboot_list(interface: str):
+    loop = asyncio.get_event_loop()
+    sock = None
+    try:
+        sock = CanSocket(loop)
+        loop.run_until_complete(sock.run_query(interface))
+    except Exception:
+        raise cli.Error('Can query error')
+    finally:
+        if sock is not None:
+            sock.close()
+
+    print(cli.success("Query Complete"))
