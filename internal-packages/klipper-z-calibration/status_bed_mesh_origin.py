c7a736a2b0f3cc2480944422db06be17f77e57f1
vertex <adam.makswiej@gmail.com>
2024-03-30 01:55:09 +0100

    update bed_mesh_origin

--- internal-packages/klipper-z-calibration/status_bed_mesh_origin.py
+++ internal-packages/klipper-z-calibration/status_bed_mesh_origin.py
@@ -1,10 +1,10 @@
 class BedMeshOrigin:
     def __init__(self, config):
-        printer = config.get_printer()  # type: Printer
-        self.bed_mesh = printer.load_object(config, 'bed_mesh')  # type: BedMesh
+        self.printer = config.get_printer()  # type: Printer
 
     def get_status(self, *_):
-        origin = self.bed_mesh.bmc.origin
+        bed_mesh = self.printer.lookup_object(config, 'bed_mesh')  # type: BedMesh
+        origin = bed_mesh.bmc.origin
         return {
             "tuple": origin,
             "text": origin[0] + "," + origin[1]
