c3b5c82411b91fa979bf3787f3347f6eee163824
vertex <adam.makswiej@gmail.com>
2024-05-03 23:33:19 +0200

    pass logger to flasher

--- mcu/provider/flash/canboot/__init__.py
+++ mcu/provider/flash/canboot/__init__.py
@@ -24,7 +24,7 @@ class CanUpdater(Flasher):
         if not os.path.isfile(firmware_path):
             raise ValueError(f"Invalid firmware path '{firmware_path}'")
 
-        with CanSocket(asyncio.get_event_loop(), mcu.interface) as sock:
+        with CanSocket(asyncio.get_event_loop(), mcu.interface, stdout_callback=stdout_callback) as sock:
             uuid = int(mcu.can_id, 16)
             await sock.bootloader(uuid)
             await sock.flash(uuid, firmware_path)
