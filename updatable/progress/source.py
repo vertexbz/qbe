6199bf6d38365b542eb29691282a4cf3743b2ded
vertex <adam.makswiej@gmail.com>
2024-05-03 17:52:53 +0200

    CLI output tweaks

--- updatable/progress/source.py
+++ updatable/progress/source.py
@@ -22,15 +22,15 @@ class SourcesProgress:
             self._parent.mark_changed()
 
     def log(self, message: str) -> None:
-        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_log(message))
+        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_log(message), _is_toplevel=False)
 
     def log_changed(self, message: str) -> None:
         self._changed = True
-        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_changed(message))
+        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_changed(message), _is_toplevel=False)
 
     def log_removed(self, message: str) -> None:
         self._changed = True
-        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_removed(message))
+        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_removed(message), _is_toplevel=False)
 
     def log_unchanged(self, message: str) -> None:
-        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_unchanged(message))
+        return self._parent.log(self._formatter.format_data_source(self._data_source) + self._formatter.format_unchanged(message), _is_toplevel=False)
